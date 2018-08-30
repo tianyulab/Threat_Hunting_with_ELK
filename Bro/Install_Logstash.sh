f975861251b40957ff4d7e918ca41de54b5b4fc6
Zer0d0y <Zer0d0y@tianyulab.com>
2018-08-30 17:02:15 +0800

    Add Install_Logstash.sh

---
+++ Bro/Install_Logstash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "安装Logstash"
+
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo apt-get -y install apt-transport-https
+echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+sudo apt-get update && sudo apt-get -y install logstash 
+systemctl status logstash.service
+
