b4391b3f8a1ac8113b059e7ad1ccb03fd6a46760
Zer0d0y <Zer0d0y@tianyulab.com>
2018-09-03 17:09:17 +0800

    Fix Deploy_Bro_Filebeat_Logstash.sh

--- Bro/Deploy_Bro_Filebeat_Logstash.sh
+++ Bro/Deploy_Bro_Filebeat_Logstash.sh
@@ -6,5 +6,5 @@ cd /etc/logstash/conf.d
 wget https://github.com/tianyulab/Threat_Hunting_with_ELK/raw/master/Bro_Filebeat_conf/Bro_Filebeat_Logstash.conf
 
 echo "启动Logstash服务：systemctl start logstash.service"
-systemctl start logstash.service
+# systemctl start logstash.service
 
